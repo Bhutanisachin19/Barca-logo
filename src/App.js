6f1b3dc18c90aaa0c8308c1ff281b184a707072f
Bhutanisachin19 <sachin.bhutani60@gmail.com>
2020-05-24 02:40:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React from 'react';
+import logo from './barca.png'; //we are using logo of react
+import './App.css'; //importing css file
+
+/*
+functional based component
+easy to create , create a function and export it out
+after exporting it is handled by index.js 
+*/
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //react app expects us to return atleast one component/element
+    //this is not html this is jsx , whatever we start we have to end it even br like <br /> (selfclosing)
+    //jsx elements must have one parent element because return only returns one so we wrapup all tags in a div tag
+
+    <div className= "App"> 
+        <header className= "App-header">
+          <img src={logo} className="App-logo"/>
+          <p>VISCA EL BARCA </p>
+          <p>MES QUE UN CLUB </p>
+        </header>
     </div>
   );
 }
